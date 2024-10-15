704f116f0a3cdb268f4d9389921a1a32d619c381
beanteacher <99578261+beanteacher@users.noreply.github.com>
2024-10-15 16:12:49 +0900

    js

--- 12_javascrpit/js-lecture-source/01_javascript-core/01_variable/01_data-type/03_boolean.js
+++
@@ -1,4 +0,0 @@
-var flag = true;
-console.log(flag);
-flag = false;
-console.log(flag);
--- 12_javascrpit/js-lecture-source/01_javascript-core/04_function/01_function-definition/01_function-declaration.js
+++
@@ -1,6 +0,0 @@
-// 1. 함수 선언문
-function hello(name) {  // 함수의 이름을 생략할 수 없다
-    return `${name}님 안녕하세요~`;
-}
-
-console.log(hello('홍길동'));
